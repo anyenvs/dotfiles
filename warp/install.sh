2e21b0b491d28b4a0234c810c559f8d42d434735
Vlad M <vlad.malinovskyy@ibm.com>
2022-11-20 16:45:56 -0500

    Add warp

---
+++ warp/install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#set -e
+ls *.env &>/dev/null && . *.env || true
+#DOTFILES_PATH=${DOTFILES_PATH:-$(dirname $(realpath ${BASH_SOURCE:-$0}))}
+DOTFILES_PATH="${DOTFILES_PATH:-$(realpath ..)}"
+test -z "${A_COMMON_FUNCTIONS}" && A_COMMON_FUNCTIONS=($(find "${DOTFILES_PATH}"/ -name a_common_functions.bash 2>/dev/null));. $A_COMMON_FUNCTIONS
+test -n "$A_COMMON_FUNCTIONS" || _error "===> ❌ A_COMMON_FUNCTIONS env var is missing in $0"
+
+_warp_install() {
+    echo -e "
+    ## https://github.com/warpdotdev
+    ## https://www.warp.dev/blog/how-to-open-warp-vscode
+    ## git clone https://github.com/warpdotdev/themes.git warp-themes
+    "
+    eval which brew && brew install --cask warp || _error "===> ❌ BREW not installed..."
+}
+_warp_themes() {
+    test -d "${DOTFILES_PATH}/warp" && ( cd $HOME ; ln -svnf "${DOTFILES_PATH}"/warp .warp ) ;
+}
+
+__main__() {
+    _log 'Warp Terminal' ;
+    _warp_install ;
+    _warp_themes ;
+}
+# ######
+# Main
+# ######
+test -n "$1" && $1 || __main__
+set +x
