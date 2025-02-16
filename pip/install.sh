1ea037823cacf136e8c43caa0c8a180e9e486f68
Vlad_M <vyacheslav.malinovskyy@ibm.com>
2025-02-15 23:06:02 -0500

    Improve to scripts

--- pip/install.sh
+++ pip/install.sh
@@ -3,9 +3,13 @@
 [ -n "${DEBUG}" ] && { export PS4='+ [${BASH_SOURCE##*/}:${LINENO}] ' ; set -x; }
 #set -e
 ls *.env &>/dev/null && . *.env || true
-DOTFILES_PATH="${DOTFILES_PATH:-$(readlink -f ..)}"
-test -z "${_HELPERS}" && _HELPERS=($(find "${DOTFILES_PATH}"/ -name _helpers.bash 2>/dev/null |sort));. $_HELPERS
+#DOTFILES_PATH="${DOTFILES_PATH:-$(readlink -f ..)}"
+BASH_SOURCE_DIR="$(dirname $(readlink -f ${BASH_SOURCE:-$0}))" BASH_SOURCE_DIR_UP="$(readlink -f $(dirname $(readlink -f ${BASH_SOURCE:-$0}))/../)";
+DOTFILES_PATH="$( readlink -f $(ls -d ${DOTFILES_PATH:-$BASH_SOURCE_DIR}/_helpers 2>/dev/null || ls -d ${DOTFILES_PATH:-$BASH_SOURCE_DIR_UP}/_helpers 2>/dev/null) | xargs dirname )";
+test -z "${_HELPERS}" && _HELPERS=($(find "${DOTFILES_PATH}"/ -name _helpers.bash 2>/dev/null |sort));
+for h in ${_HELPERS[@]} ;do . "$h" ;done
 test -n "$_HELPERS" || _error "===> ❌ _HELPERS env var is missing in $0"
+echo _HELPERS = "${_HELPERS[@]}"
 
 # ######
 # PyEnv Dev Dependencies
